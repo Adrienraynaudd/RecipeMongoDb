2cd7eaa8671595304a509449efe9335ba9909051
MaxChevalier <maxime.chevalier@ynov.com>
2024-02-19 16:41:32 +0100

    ✨add recipe feat

---
+++ Models/Recipe.js
@@ -0,0 +1,24 @@
+const e = require('express');
+const mongoose = require('mongoose');
+
+const IngredientsSchema = mongoose.Schema({
+    name: {type: String, required: true},
+    quantity: {type: Number, required: true},
+    unit: {type: String, required: true, enum: ['g', 'kg', 'ml', 'cl', 'l', 'cuillère à soupe', 'cuillère à café', 'verre', 'tasse', 'bol', 'pincée', 'pincées', 'pincées']}
+});
+
+const RecipeSchema = mongoose.Schema({
+    title: {type:String, required: true},
+    image: {type:String},
+    ingredients: {type:[IngredientsSchema], required: true},
+    steps: {type: [String], required: true},
+    cookingTime: {type: Number, required: true},
+    servings: {type: Number, required: true},
+    date: {type: Date, default: Date.now},
+    // user: {type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true},
+    desc: {type: String, required: true},
+    tags: {type: [String], required: true},
+    difficulty: {type: String, required: true, enum: ['Facile', 'Moyen', 'Difficile']},
+});
+
+module.exports = mongoose.model('Recipe', RecipeSchema);
--- server.js
+++ server.js
@@ -1,10 +1,29 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const compression = require('compression');
+const bodyParser = require('body-parser');
+
+const RecipeRouter = require('./Routes/Recipe');
+
 const app = express();
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Serveur en cours d'exécution sur le port ${PORT}`);
 });
+
+app.use(compression());
+
+mongoose.connect("mongodb://localhost:27017").then(() => {
+  console.log('Connected to MongoDB with Success !');
+}).catch((err) => {
+  console.log('MongoDB ERROR', err);
+});
+
+app.use(bodyParser.json());
+
+app.use('/api/recipes', RecipeRouter);
+
 app.get('/', (req, res) => {
     res.send('Bonjour, monde !');
   });
